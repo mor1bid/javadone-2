fc9a161c26b091115709fc22f728bc5d306b14a7
De <spark49942@gmail.com>
2022-11-27 02:47:28 +0300

    DZ2-0

--- dz.java
+++ dz.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.util.logging.*;
 // import java.io.File;
 import java.io.PrintWriter;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * dz
@@ -12,6 +13,35 @@ import java.io.PrintWriter;
 public class dz {
     public static void main(String[] args) throws IOException {
         Scanner work = new Scanner(System.in);
+        System.out.println("2. Введите желаемый размер массива: ");
+        String inp = work.nextLine();
+        int si = Integer.parseInt(inp);
+        int[] ray = new int[si];
+        System.out.print("[");
+        for (int i = 0; i<si; i++) 
+        {
+            int randnum = ThreadLocalRandom.current().nextInt(0, 10);
+            ray[i] = randnum;
+            System.out.print(ray[i] + ", ");
+        }
+        System.out.print("] - изначальный вид массива\n[");
+        int sel = ray[0], temp = 0;
+        int[] array = new int[si];
+        for (int i = 0; i<si; i++) {
+            for (int j = 0; j<si; j++) {
+                if (ray[i]<ray[j]) { sel = ray[i]; }
+                }
+            array[i] = sel;
+            System.out.print(ray[i] + ", ");
+        }
+        System.out.print("] - сортированный вид массива");
+        Logger spy = Logger.getLogger(dz.class.getName());
+        FileHandler fiha = new FileHandler("log.txt");
+        spy.addHandler(fiha);
+
+        SimpleFormatter sFormat = new SimpleFormatter();
+        fiha.setFormatter(sFormat);
+
         System.out.println("\n4. Калькулятор! \nДоступные арифметические действия: сложение (+), вычитание (-), умножение (*), деление (/) \nВведите первый аргумент: ");
 		String arga = work.nextLine();
 		double numa = 0, numb = 0;
@@ -28,23 +58,9 @@ public class dz {
 		else if (opera.equals("-")) { double res = numa - numb; System.out.println("Ответ: " + res); }
 		else if (opera.equals("*")) { double res = numa * numb; System.out.println("Ответ: " + res); }
 		else if (opera.equals("/") && numb != 0) { double res = numa / numb; System.out.println("Ответ: " + res); }
-		else System.out.println("Ошибка!");
+		else { System.out.println("Ошибка!"); spy.log(Level.WARNING, "Something went wrong!"); }
         work.close();
 
-        Logger spy = Logger.getLogger(dz.class.getName());
-        //ConsoleHandler coha = new ConsoleHandler();
-        PrintWriter writer = new PrintWriter("log.txt", "UTF-8");
-        FileHandler fiha = new FileHandler("log.txt");
-        //spy.addHandler(coha);
-        spy.addHandler(fiha);
-        
-        SimpleFormatter sFormat = new SimpleFormatter();
-        //XMLFormatter xml = new XMLFormatter();
-        fiha.setFormatter(sFormat);
-        //fiha.setFormatter(xml);
-        
-        //spy.setLevel(Level.INFO);
-        spy.log(Level.WARNING, "Тестовое логирование 1");
-        spy.info("Тестовое логирование 2");
+        spy.info("Log");
     }
 }
